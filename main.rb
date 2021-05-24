ec444235f6a23e8a4c1423cdbced47ff7e6025c2
qPasta <67769992+Dmartinez-van@users.noreply.github.com>
2021-05-24 12:30:49 -0700

    setup game break out loop condition

--- main.rb
+++ main.rb
@@ -1,10 +1,9 @@
-require "./Game.rb"
-require "./Player.rb"
-require "./Question.rb"
-
-# this file will continue the main game loop
+require './Game.rb'
+require './Player.rb'
+require './Question.rb'
 
 game = Game.new
+
 
 while game.game_on
   
@@ -13,7 +12,22 @@ while game.game_on
 
 
 
-
-  # Check for exit condition
-  game.game_over?
+  
+  player1.life -= 1
+#   # check for game over condition
+  if player1.life == 0
+    puts "Player 1 loses!"
+    puts "Final Score!!"
+    puts "P1: #{player1.life}/3 vs P2: #{player2.life}/3"
+    game.game_on = false
+  elsif player2.life == 0
+    puts "Player 2 loses!"
+    puts "Final Score!!"
+    puts "P1: #{player1.life}/3 vs P2: #{player2.life}/3"
+    game.game_on = false
+  else
+    puts "Current Score"
+    puts "P1: #{player1.life}/3 vs P2: #{player2.life}/3"
+    puts "------ New Turn ------"
+  end
 end
